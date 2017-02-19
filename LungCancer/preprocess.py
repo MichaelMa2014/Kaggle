efb3792d98f53dd3993c74018f80a26f062c44ba
MichaelMa <Minima2014@iCloud.com>
2017-02-19 18:00:52 +0800

    The big blue box is due to set_xlim and other 2, not sure why

--- LungCancer/preprocess.py
+++ LungCancer/preprocess.py
@@ -30,7 +30,7 @@ def load_scan(path):
     for s in slices:
         s.SliceThickness = slice_thickness
 
-    # np.array() should also work according to the preceding cells
+    # np.array() should also work
     image = np.stack([s.pixel_array for s in slices])
     # Convert to int16 (from sometimes int16),
     # should be possible as values should always be low enough (<32k)
@@ -65,17 +65,17 @@ def plot_3d(patient_scan, threshold=-300):
     """
     # Position the scan upright,
     # so the head of the patient would be at the top facing the camera
-    p = patient_scan.transpose(2,1,0)
+    p = patient_scan.transpose(2, 1, 0)
 
     verts, faces = measure.marching_cubes(p, threshold)
-
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(111, projection='3d')
 
     # Fancy indexing: `verts[faces]` to generate a collection of triangles
     mesh = Poly3DCollection(verts[faces], alpha=0.1)
     face_color = [0.5, 0.5, 1]
     mesh.set_facecolor(face_color)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
     ax.add_collection3d(mesh)
 
     ax.set_xlim(0, p.shape[0])
