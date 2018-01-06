ea6fe4705163af2f4a1b25bac09784f27aa7b0d8
Michael Ma <Minima2014@iCloud.com>
2018-01-06 19:09:53 +0800

    Ada boost with decision tree. Graph of tree

--- sf_crime/decision_tree.py
+++ sf_crime/decision_tree.py
@@ -1,5 +1,9 @@
+import os
 import sklearn.tree
+import sklearn.metrics
+import sklearn.ensemble
 
+from util import OUTPUT_PATH
 from data import DataHandler
 
 d = DataHandler()
@@ -7,8 +11,23 @@ print(d.train)
 print(d.target)
 print(d.test)
 
-classifier = sklearn.tree.DecisionTreeClassifier()
+classifier = sklearn.tree.DecisionTreeClassifier(criterion="entropy", max_leaf_nodes=39)
 classifier.fit(d.train, d.target)
 prob = classifier.predict_proba(d.test)
-d.save_submission(prob)
+d.save_submission(prob, "decision_tree")
+print("decision_tree: Train set acc:", classifier.score(d.train, d.target))
+print("decision_tree: Train set loss:", sklearn.metrics.log_loss(d.target, classifier.predict_proba(d.train)))
+sklearn.tree.export_graphviz(
+        classifier,
+        out_file=os.path.join(OUTPUT_PATH, "tree.dot"),
+        label="none",
+        impurity=False,
+        filled=True)
+
+ada_classifier = sklearn.ensemble.AdaBoostClassifier(classifier)
+ada_classifier.fit(d.train, d.target)
+prob = ada_classifier.predict_proba(d.test)
+d.save_submission(prob, "decision_tree_ada")
+print("decision_tree_ada: Train set acc:", ada_classifier.score(d.train, d.target))
+print("decision_tree_ada: Train set loss:", sklearn.metrics.log_loss(d.target, ada_classifier.predict_proba(d.train)))
 
