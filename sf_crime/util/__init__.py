ac3ce3f8a0ef4f19c3a10312cb801b3c7ec91a29
Michael Ma <Minima2014@iCloud.com>
2018-01-06 19:11:11 +0800

    Helper function to inspect variables

--- sf_crime/util/__init__.py
+++ sf_crime/util/__init__.py
@@ -1,6 +1,15 @@
 import os
+import inspect
 
 ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 INPUT_PATH = os.path.join(ROOT_PATH, "data")
 OUTPUT_PATH = os.path.join(ROOT_PATH, "data")
+
+
+def print_name_and_value(var):
+    frame = inspect.stack()[1]
+    # list(frame) is [<frame object at 0x7f69511bf828>, '__init__.py', 20, '<module>', ['print_name_and_value(a)\n'], 0]
+    lines = frame[4]
+    var_name = ''.join(lines).strip().split('(')[-1].split(')')[0]
+    print("%s: %d" % (var_name, var))
 
