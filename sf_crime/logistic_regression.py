0b35be2949e13cdf80515b56d03da715e673b404
Michael Ma <Minima2014@iCloud.com>
2018-01-07 16:06:23 +0800

    More results on logistic_regression

--- sf_crime/logistic_regression.py
+++ sf_crime/logistic_regression.py
@@ -9,12 +9,12 @@ print(d.target)
 print(d.test)
 
 classifier = sklearn.linear_model.LogisticRegression(
-        penalty="l2",
-        solver="newton-cg",
-        multi_class="multinomial",
+        penalty="l1",
+        solver="liblinear",
+        multi_class="ovr",
         n_jobs=-1)
 classifier.fit(d.train, d.target)
 prob = classifier.predict_proba(d.test)
-d.save_submission(prob, "logistic_regression_newton-cg_multinomial")
+d.save_submission(prob, "logistic_regression_l1")
 print("logistic_regression: Train set acc:", classifier.score(d.train, d.target))
 print("logistic_regression: Train set loss:", sklearn.metrics.log_loss(d.target, classifier.predict_proba(d.train)))
